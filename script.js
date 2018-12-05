f7b4ba6d9222dc24f7dc91915cb03b9226d833f1
CDbrooks <christiandbrooks@gmail.com>
2018-12-05 13:30:27 -0500

    New design

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    $("#myName").fadeIn( 700, () => {
+        $("#heroFirst").fadeIn( 700, () => {
+            $("#heroSecond").fadeIn( 700, () => {
+            })
+        })
+    })
+})
